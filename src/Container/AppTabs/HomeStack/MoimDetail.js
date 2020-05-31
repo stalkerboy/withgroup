301185cb9c2b80cd9a6bf6c2253f6a2000bf409b
최성준 <csj2702@naver.com>
2020-05-31 14:26:17 +0900

    모임디테일 퍼블리싱

--- src/Container/AppTabs/HomeStack/MoimDetail.js
+++ src/Container/AppTabs/HomeStack/MoimDetail.js
@@ -33,104 +33,134 @@ export function MoimDetail({route, navigation}) {
 
   return (
     <View style={styles.container}>
-        <Text style={styles.top}>스터디 목표</Text>
- 
-        <Text style={styles.top}>모임원 보기</Text>
-        
-        <Text style={styles.top}>일정 달성 현황</Text>
-        <View style={styles.rectangle}> 
-                                          <TouchableOpacity onPress={() => navigation.navigate('SharePlan')}>
-                                            <Image source={require('../../../../assets/images/moimdetail/SharePlan.png')} style={styles.RectangleList}/>
-                                          </TouchableOpacity>
-                                          <TouchableOpacity onPress={() => navigation.navigate('SharePlan')}>
-                                            <Text style={styles.RectangleList}>일정</Text>
-                                          </TouchableOpacity>
-                                          <Image source={require('../../../../assets/images/moimdetail/Chat.png')} style={styles.RectangleList}/>
-                                          <Text style={styles.RectangleList}>채팅</Text>
-        </View>
-    </View>);
+      <View style={styles.rect}>
+        <Image
+          source={require("../../../../assets/images/moimdetail/Moimdetail1.png")}
+          resizeMode="contain"
+          style={styles.image3}
+        ></Image>
+        <Text style={styles.계획공유}>계획 공유</Text>
+        <Image
+          source={require("../../../../assets/images/moimdetail/Plan_Share.png")}
+          resizeMode="contain"
+          style={styles.image4}
+        ></Image>
+        <Text style={styles.모임원}>모임원</Text>
+        <Image
+          source={require("../../../../assets/images/moimdetail/Chat.png")}
+          resizeMode="contain"
+          style={styles.image2}
+        ></Image>
+        <Text style={styles.채팅}>채팅</Text>
+      </View>
+      <Text>
+       
+      </Text> 
+      <Text style={styles.loremIpsum}>
+     
+      </Text>
+      <View style={styles.imageRow}>
+        <Image
+          source={require("../../../../assets/images/moimdetail/Moimdetail1.png")}
+          resizeMode="contain"
+          style={styles.image}
+        ></Image>
+        <Text style={styles.스터디목표}>스터디 소개</Text>
+      </View>
+    </View>
+    );
 }
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5F5F5',
+    flex: 1
   },
-  RectangleList:{
-    marginLeft:10,
-    marginTop:20,
-  },
-  rectangle: {
-    marginBottom: 136,
-    marginLeft:350,
-    paddingVertical: 50,
-    width: 50,
-    borderRadius:5,
-    shadowColor: '#000000',
+  rect: {
+    width: 70,
+    height: 269,
+    shadowOpacity: 1,
+    backgroundColor: "rgba(15,15, 15,0)",
+    borderWidth: 4,
+    borderColor: "rgba(230,224,224,1)",
+    shadowColor: "rgba(253,244,244,1)",
     shadowOffset: {
-      width: 0,
-      height: 3
+      width: 5,
+      height: 5
     },
-    shadowRadius: 5,
-    shadowOpacity: 1.0,
-    elevation: 1,
+    elevation: 0,
+    marginTop: 334,
+    marginLeft: 308
+  },
+  image3: {
+    width: 58,
+    height: 41,
+    marginTop: 12,
+    marginLeft: 7
+  },
+  계획공유: {
+    fontFamily: "comic-sans-ms-regular",
+    color: "#121212",
+    height: 17,
+    width: 48,
+    fontSize: 12,
+    marginTop: 5,
+    marginLeft: 11
+  },
+  image4: {
+    width: 54,
+    height: 54,
+    marginTop: 23,
+    marginLeft: 8
+  },
+  모임원: {
+    fontFamily: "comic-sans-ms-regular",
+    color: "#121212",
+    height: 20,
+    width: 39,
+    fontSize: 12,
+    marginLeft: 15
+  },
+  image2: {
+    width: 48,
+    height: 44,
+    marginTop: 20,
+    marginLeft: 11
+  },
+  채팅: {
+    fontFamily: "comic-sans-ms-regular",
+    color: "#121212",
+    height: 22,
+    width: 59,
+    fontSize: 12,
     textAlign: "center",
-    fontSize: 30,
+    marginTop: 11,
+    marginLeft: 11
   },
-  top:{
-    color: "#03D2B4"
-    ,fontSize: 20
-    ,fontWeight: "bold"
-    ,flex: 1
-    ,marginTop:10
-    ,marginLeft:10
- 
+  loremIpsum: {
+    fontFamily: "roboto-regular",
+    color: "#121212",
+    height: 85,
+    width: 247,
+    marginTop: -550,
+    marginLeft: 106
   },
-  moimButton: {
-    width: 100,
-    height: 25,
-    borderWidth: 1,
-    borderColor: 'gray',
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: 34,
+    height: 34
   },
-  peopleList: {
-    color: 'white',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center',
-    flex: 1,
-    fontSize: 30,
+  스터디목표: {
+    fontFamily: "comic-sans-ms-regular",
+    color: "rgba(3,210,180,1)",
+    height: 36,
+    width: 190,
+    textAlign: "left",
+    fontSize: 25,
+    marginLeft: 23
   },
-  modal: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.2)',
-    height: '80%',
-    width: '80%',
-    borderRadius: 10,
-    borderWidth: 1,
-    borderColor: '#fff',
-    marginTop: 80,
-    marginLeft: 40,
-    flexDirection: 'column',
-  },
-  friends: {
-    padding: 5,
-  },
-  nameText: {
-    fontSize: 20,
-  },
-  intro: {
-    alignItems: 'center',
-    justifyContent: 'space-between',
-    // width: width - 10,
-    height: 150,
-    borderWidth: 3,
-    borderColor: 'gray',
-    marginTop: 3,
-    flexDirection: 'column',
-  },
+  imageRow: {
+    height: 42,
+    flexDirection: "row",
+    marginTop: 17,
+    marginLeft: 66,
+    marginRight: 62
+  }
 });
